e00c85b670f32fd92aeb075c6c48caa3b86ae0d2
Eli Wang <eliwangc@gmail.com>
2023-01-29 22:29:29 -0800

    Complete blocker.js

--- blocker.js
+++ blocker.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 
 const filePath = "/etc/hosts";
 const redirectPath = "127.0.0.1";
-let websites = [ "www.youtube.com","www.zhihu.com" ];
+let websites = [ "www.youtube.com","www.zhihu.com", "www.bilibili.com"];
 let delay = 10000;
 
 
 let blocker = () => {
-	let date = new date();
+	let date = new Date();
 	let hours = date.getHours();
+	let minutes = date.getMinutes();
 	
-	// Block the websites when it's later than 8pm or earlier than 8am.
-	if(hours >= 20 || hours < 8) {
+	// Block the websites when it's later than 5pm or earlier than 7am.
+	if(hours >= 17 || hours < 7) {
 		console.log('Time to block websites');
 		fs.readFile(filePath, (err, data) => {    
 			fileContents = data.toString();    
@@ -25,7 +26,7 @@ let blocker = () => {
 						if (err)  return console.log(err);                
 						console.log('File Updated Successfully');
 					});        
-				} 
+				}
 				else {            
 					console.log('Website is present');        
 				}    
@@ -34,5 +35,33 @@ let blocker = () => {
 
 	} 
 	else {
-		console.log('Time to unblock websites');    ....    ....}
+		console.log('Time to unblock websites');
+		// Initialize the empty string
+		let completeContent = '';
+		// Read the file line by line
+		fs.readFileSync(filePath) .toString() .split() .forEach((line) => {
+			// Replace the file contents by `completeContent` variable
+			fs.writeFile(filePath, completeContent, (err) => {    
+				if (err) {        
+					return console.log('Error!', err);    
+				}
+			});
+
+			let flag = 1;
+			for (let i = 0; i < websites.length; i++) {    
+				if (line.indexOf(websites[i]) >= 0) {  // line contains website
+					flag = 0;        
+					break;    
+				}				
+			}
+			if(flag == 1) {    
+				if (line === '')    completeContent += line;    // If last line
+				else    completeContent += line + "\n";			// Not last line
+			}
+
+		})
+	}
 };
+
+blocker(); 						// Need to run once before setInterval	
+setInterval(blocker, delay);  	// Keeps repeating the blocker function after a specific time interval
